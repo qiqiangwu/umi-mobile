5f2847e003bff0975f4aff7277ca66a72b14bc4c
qiqiangwu <qiqiangwu@gmail.com>
2021-11-04 14:06:36 +0800

    feat: 引入`js-Logger`日志库

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { connect, Link } from 'umi';
+import Logger from '@/utils/logger';
+
+const logger = Logger.get('/home');
 
 const HomePage: React.FC = props => <Link to="/home/123">导航</Link>;
 
---
+++ src/utils/logger.ts
@@ -0,0 +1,22 @@
+/**
+ * 打印日志
+ *
+ * @example
+ * 命名日志
+ * const logger = Logger.get('/home');
+ * logger.debug('I am a debug message');
+ * logger.info('OMG! Check this window out!');
+ * logger.warn('Purple Alert! Purple Alert!');
+ * logger.error('HOLY SHI... no carrier.');
+ */
+import Logger from 'js-logger';
+
+Logger.useDefaults({
+  defaultLevel: Logger.DEBUG,
+});
+
+Logger.setHandler((messages, context) => {
+  // Send messages to a custom logging endpoint for analysis.
+});
+
+export default Logger;
